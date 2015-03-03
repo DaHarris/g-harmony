f3df157c05d6eb95b68f2452c961aee611a6d561
nina <ninajl@gmail.com>
2015-03-03 15:27:07 -0700

    Create User ability to log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,24 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    if session[:user_id]
+      User.find(session[:user_id])
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
 end
